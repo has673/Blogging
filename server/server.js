53ae27e2880ece1be2bb9d87fe41225c2a964c26
has673 <hassansajjas>
2024-01-23 13:57:37 +0500

    'fff'

---
+++ server/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const cors = require('cors');
+const{connectDB} = require('./config/db');
+const path = require('path');
+const app = express();
+const port = process.env.PORT || 3000;
+const cookieParser = require('cookie-parser');
+require('dotenv').config();
+connectDB()
+app.listen(port, () => {
+          console.log(`Server is running on port: ${port}`);
+        });
+      
+
+app.use(cors());
+app.use(express.json());
+// app.use('/uploads', express.static(__dirname + '/uploads'))
+app.use(cookieParser())
