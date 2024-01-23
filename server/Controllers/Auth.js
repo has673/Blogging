7bff30fc1b625f345472e13f468f3f15bf2eb2db
has673 <hassansajjas>
2024-01-23 16:19:31 +0500

    node modlues

--- server/Controllers/Auth.js
+++ server/Controllers/Auth.js
@@ -13,7 +13,7 @@ const jwt = require('jsonwebtoken');
     const newuser = new User({...req.body , password:newpass})
     await newuser.save()
     console.log('New User Added')
-    return res.status(200).json({message:"User added"}, newuser)
+    return res.status(200).json({ message: "User added", user: newuser });
 
    }
    catch{
