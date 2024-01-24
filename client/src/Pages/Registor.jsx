9f6f8a935f60a71e98ffb0e2d51bbd2e75ade37a
has673 <hassansajjas>
2024-01-24 15:21:50 +0500

    login signup

---
+++ client/src/Pages/Registor.jsx
@@ -0,0 +1,61 @@
+import React, { useState } from 'react'
+import axios from 'axios'
+
+function Registor() {
+    const initialState = {
+       
+        email:'',
+        password:'',
+       
+      };
+    const [userdata , setUserdata]= useState(initialState)
+    function onchangetext ( e){
+        const{name , value} = e.target
+        setUserdata(prevUserdata =>{
+            return{
+                ...prevUserdata,
+                [name]: value
+                
+            }
+    
+        })
+     
+    }
+   async   function HandleSubmit(e){
+    e.preventDefault();
+    try {
+        const res = await axios.post('http://localhost:3000/auth/signup', {
+         
+          email: userdata.email,
+          password: userdata.password,
+        });
+  
+        // Handle the response here, like showing a success message or redirecting
+        console.log('Response from server:', res.data);
+        // toast.success(res.data.message)
+        // navigate('/login')
+      } catch (error) {
+        // Handle errors here, like showing an error message
+        console.error('Error:', error.message);
+      }
+  
+    }
+  return (
+    <div>
+        <h1>Signup</h1>
+        <form>
+            <div>
+                <input placeholder='Email' type='text'  name='email' value={userdata.email} onChange={onchangetext}>
+                </input>
+            </div>
+            <div>
+                <input placeholder='Password' type='password'>
+                </input>
+            </div>
+            <input type='submit' onClick={HandleSubmit}>Registor</input>
+        </form>
+    </div>
+  )
+}
+
+export default Registor
