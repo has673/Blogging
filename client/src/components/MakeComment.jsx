9915bfd39b7feca714d1f73dc19c42e5e993ecb1
has673 <hassansajjas>
2024-04-02 14:24:46 +0500

    some_ui

--- client/src/components/MakeComment.jsx
+++ client/src/components/MakeComment.jsx
@@ -10,7 +10,7 @@ function MakeComment({ content, handleSubmit, setContent }) {
        
           <div className="w-full space-y-3 text-gray-700">
             <div className="">
-              <textarea value={content} onChange={(e) => setContent(e.target.value)} placeholder="Write your comment here" cols="30" rows="6" className="h-40 w-full min-w-full max-w-full overflow-auto whitespace-pre-wrap rounded-md border bg-white p-5 text-sm font-normal normal-case text-gray-600 opacity-100 outline-none focus:text-gray-600 focus:opacity-100 focus:ring"></textarea>
+              <textarea value={content} onChange={(e) => setContent(e.target.value)} placeholder="Write your comment here" cols="20" rows="6" className="h-40 w-full min-w-full max-w-full overflow-auto whitespace-pre-wrap rounded-md border bg-white p-5 text-sm font-normal normal-case text-gray-600 opacity-100 outline-none focus:text-gray-600 focus:opacity-100 focus:ring"></textarea>
             </div>
             <div className="float-right">
               <button onClick={handleSubmit} className=" cursor-pointer p-3 bg-green-500 text-white rounded-xl  hover:bg-green-600">Post Comment</button>
