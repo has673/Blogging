a97447031270131a806e164e850469c90b3ffdc7
has673 <hassansajjas>
2024-04-02 12:11:16 +0500

    ui_change

--- client/src/components/BlogCard.jsx
+++ client/src/components/BlogCard.jsx
@@ -6,20 +6,21 @@ function BlogCard({ blog }) {
     <div className="max-w-sm rounded w-100 overflow-hidden shadow-lg">
       <Link to={`/blog/${blog._id}`} className="block">
         <h2 className="text-xl font-semibold mb-2">{blog.title}</h2>
-        {blog.photo ? (
-          <img
-            src={`http://localhost:3000/blog/getblogphoto/${blog._id}`}
-            alt={`Photo for ${blog.title}`}
-            className="w-auto h-25 text-center"
-          />
-        ) : (
-          <div className="bg-gray-300 w-full h-40 flex items-center justify-center">
-            <p>No Image Available</p>
-          </div>
-        )}
+        <div className="h-40">
+          {blog.photo ? (
+            <img
+              src={`http://localhost:3000/blog/getblogphoto/${blog._id}`}
+              alt={`Photo for ${blog.title}`}
+              className="w-full h-full object-cover"
+            />
+          ) : (
+            <div className="bg-gray-300 w-full h-full flex items-center justify-center">
+              <p>No Image Available</p>
+            </div>
+          )}
+        </div>
         <div className="mt-2">
-        <p className="text-gray-700 text-base">{`Likes: ${blog.likes}`}</p>
-
+          <p className="text-gray-700 text-base">{`Likes: ${blog.likes}`}</p>
         </div>
       </Link>
     </div>
--- client/src/components/MyBlogCard.jsx
+++ client/src/components/MyBlogCard.jsx
@@ -2,25 +2,26 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { MdDelete } from "react-icons/md";
 
-function MyBlogCard({ blog  , ondelete}) {
+function MyBlogCard({ blog, ondelete }) {
   return (
-    <div className="max-w-sm rounded overflow-hidden shadow-lg m-5">
+    <div className="max-w-sm rounded w-100 overflow-hidden shadow-lg mx-8 my-4"> {/* Added mx-8 for horizontal margin and my-4 for vertical margin */}
       <Link to={`/blog/${blog._id}`} className="block">
         <h2 className="text-xl font-semibold mb-2">{blog.title}</h2>
-        {blog.photo ? (
-          <img
-            src={`http://localhost:3000/blog/getblogphoto/${blog._id}`}
-            alt={`Photo for ${blog.title}`}
-            className="w-25 h-auto"
-          />
-        ) : (
-          <div className="bg-gray-300 w-full h-40 flex items-center justify-center">
-            <p>No Image Available</p>
-          </div>
-        )}
+        <div className="h-40">
+          {blog.photo ? (
+            <img
+              src={`http://localhost:3000/blog/getblogphoto/${blog._id}`}
+              alt={`Photo for ${blog.title}`}
+              className="w-full h-full object-cover"
+            />
+          ) : (
+            <div className="bg-gray-300 w-full h-full flex items-center justify-center">
+              <p>No Image Available</p>
+            </div>
+          )}
+        </div>
         <div className="mt-2">
-        <p className="text-gray-700 text-base">{`Likes: ${blog.likes}`}</p>
-     
+          <p className="text-gray-700 text-base">{`Likes: ${blog.likes}`}</p>
         </div>
       </Link>
       <button> <MdDelete onClick={ondelete}/> </button> 
